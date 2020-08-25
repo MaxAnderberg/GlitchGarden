cc875b2753d5e79dd50c35ffbb7bb6f4cecb6267
Max Anderberg <maxanderberg@gmail.com>
2020-08-25 21:56:12 +0200

    added a deathVFX to when the enemy dies

--- GlitchGarden/Assets/Scripts/Health.cs
+++ GlitchGarden/Assets/Scripts/Health.cs
@@ -5,14 +5,25 @@ using UnityEngine;
 public class Health : MonoBehaviour
 {
     [SerializeField] float health = 100f;
-  
+    [SerializeField] GameObject deathVFX;
+    [SerializeField] float deathTime = 1f;
+
     public void DealDamage(float damage)
     {
         health -= damage;
         if (health <= 0 )
         {
+
+            DeathVFX();
             Destroy(gameObject);
         }
+    }
+
+    private void DeathVFX()
+    {
+        GameObject death = Instantiate(deathVFX, transform.position, transform.rotation) as GameObject;
+        Destroy(death, 1f);
+
     }
 
 }
