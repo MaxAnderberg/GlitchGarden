7a83177d6859797374879c1db081e52f143edee1
Max Anderberg <maxanderberg@gmail.com>
2020-08-26 21:31:36 +0200

    A script that allows us to spawn a cactus (defender) where we click inside the collider box

---
+++ GlitchGarden/Assets/Scripts/SpawnDefender.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnDefender : MonoBehaviour
+{
+    [SerializeField] GameObject defender;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+
+
+    }
+
+    private void OnMouseDown()
+    {
+        {
+            SpawnDefenders(GetSquareClicked());
+        }
+    }
+
+    private Vector2 GetSquareClicked()
+    {
+        
+            
+            Vector2 clickPos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+            Vector2 worldPos = Camera.main.ScreenToWorldPoint(clickPos);
+            Vector2 gridPos = SnapToGrid(worldPos);
+            return gridPos;
+    }
+    
+    private Vector2 SnapToGrid(Vector2 rawWorldPos)
+    {
+        float newX = Mathf.RoundToInt(rawWorldPos.x);
+        float newY = Mathf.RoundToInt(rawWorldPos.y);
+        return new Vector2(newX, newY);
+    }
+
+    private void SpawnDefenders(Vector2 roundedPos)
+    {
+            GameObject newDefender = Instantiate(defender, roundedPos, Quaternion.identity) as GameObject;
+    }
+  
+
+}	
