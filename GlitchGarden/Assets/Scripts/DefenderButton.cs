4f1aea84e099742d49bc33d82eb8c534d72997b8
Max Anderberg <maxanderberg@gmail.com>
2020-08-27 21:57:20 +0200

    Init a script that takes care of clicking and selecting the defender button in the UI

---
+++ GlitchGarden/Assets/Scripts/DefenderButton.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DefenderButton : MonoBehaviour
+{
+    [SerializeField] Defender defenderPrefab;
+
+
+    private void OnMouseDown()
+    {
+        Debug.Log("Clicking the right area you are!");
+        var buttons = FindObjectsOfType<DefenderButton>();
+        foreach(DefenderButton button in buttons)
+        {
+            button.GetComponent<SpriteRenderer>().color = new Color32(85, 85, 85,255); // 85 85 85 
+        }
+
+        GetComponent<SpriteRenderer>().color = Color.white;
+        FindObjectOfType<SpawnDefender>().SetSelectedDefender(defenderPrefab);
+
+    }
+}
