581e03c93e74cb9177e49e10c992287963a882ab
Max Anderberg <maxanderberg@gmail.com>
2020-08-27 21:56:31 +0200

    Init defender script. Going to hold information regarding the defender like cost etc.

---
+++ GlitchGarden/Assets/Scripts/Defender.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Defender : MonoBehaviour
+{
+    [SerializeField] int coinCost = 100; // price per defender
+}
