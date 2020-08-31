c0b88e3043f10653d21f09787ba13c4ff3179bcb
Max Anderberg <maxanderberg@gmail.com>
2020-08-31 22:00:20 +0200

    Init player health - this is used so that we can actually loose the game

---
+++ GlitchGarden/Assets/Scripts/Player/PlayerHealth.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealth : MonoBehaviour
+{
+
+    [SerializeField] int playerHealth = 10; // One unit does one damage. So on collider -1
+    Text playerHealthText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerHealthText = GetComponent<Text>();
+        UpdateDisplay();
+
+    }
+
+    public int GetPlayerHealth()
+    {
+        return playerHealth;
+    }
+    // remember to set this to on trigger in Unity
+
+    private void UpdateDisplay()
+    {
+        playerHealthText.text = playerHealth.ToString();
+    }
+
+    public void TakeDamage(int amount)
+    {
+        if (playerHealth >= amount)
+        {
+            playerHealth -= amount;
+            UpdateDisplay();
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
