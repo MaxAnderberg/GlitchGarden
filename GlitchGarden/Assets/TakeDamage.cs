31809fff2d0f1eab69e5c2b16a96f3b1cf907d97
Max Anderberg <maxanderberg@gmail.com>
2020-08-31 22:01:19 +0200

    Init take damage. When an attacker reaches the far left collider we will take 1 damage

---
+++ GlitchGarden/Assets/TakeDamage.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TakeDamage : MonoBehaviour
+{
+
+    private void OnTriggerEnter2D(Collider2D otherCollider)
+    {
+        var player = FindObjectOfType<PlayerHealth>();
+       
+        if (player.GetPlayerHealth() <= 0)
+        {
+            SceneManager.LoadScene(2);
+        } else if (player.GetPlayerHealth() >= 1)
+        {
+            player.TakeDamage(1);
+            Destroy(otherCollider.gameObject, 1f);
+        }
+
+    }
+
+}
