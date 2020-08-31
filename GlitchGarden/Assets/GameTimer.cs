fdfe1212ca9c1e60f6434a7fdc23468fa4e1a086
Max Anderberg <maxanderberg@gmail.com>
2020-08-31 22:05:26 +0200

    Added a game timer

---
+++ GlitchGarden/Assets/GameTimer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameTimer : MonoBehaviour
+{
+    [Tooltip("The game time in seconds.")]
+    [SerializeField] float levelTime = 10f;
+    // Start is called before the first frame update
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetComponent<Slider>().value = Time.timeSinceLevelLoad / levelTime;
+
+        bool timerFinished = (Time.timeSinceLevelLoad >= levelTime);
+        if (timerFinished)
+        {
+            Debug.Log("Level finished");
+        }
+    }
+}
