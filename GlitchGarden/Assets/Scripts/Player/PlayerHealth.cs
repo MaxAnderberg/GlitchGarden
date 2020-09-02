57141ed553dbdf54e0d5e719dbcdba87e085b30e
Max Anderberg <maxanderberg@gmail.com>
2020-09-02 21:05:00 +0200

    Changed player's health to float instead of int

--- GlitchGarden/Assets/Scripts/Player/PlayerHealth.cs
+++ GlitchGarden/Assets/Scripts/Player/PlayerHealth.cs
@@ -5,19 +5,23 @@ using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour
 {
-
-    [SerializeField] int playerHealth = 10; // One unit does one damage. So on collider -1
+    [SerializeField] float baseHealth = 3;
+    
+    
+    float playerHealth; // 
     Text playerHealthText;
 
     // Start is called before the first frame update
     void Start()
     {
+        playerHealth = baseHealth - PlayerPrefsController.GetDifficulty();
         playerHealthText = GetComponent<Text>();
         UpdateDisplay();
+        Debug.Log("The diffuculty is: " + PlayerPrefsController.GetDifficulty());
 
     }
 
-    public int GetPlayerHealth()
+    public float  GetPlayerHealth()
     {
         return playerHealth;
     }
